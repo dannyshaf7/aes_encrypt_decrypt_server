b225cd917405f2accc9f3fb3fd36f8561c08494a
Daniel Shafer <daniel.shafer@outlook.com>
2024-04-04 03:13:56 -0400

    it's done! it fragments, it sends back and forth, ugh this was rough.

--- server.py
+++ server.py
@@ -8,7 +8,30 @@ from Crypto.Cipher import AES
 from Crypto.Hash import HMAC, SHA256
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
+import math
 import socket
+import time
+
+
+def fragment_message(message):
+   frag_count = math.ceil(len(message) / 1020)
+   # print("Number of fragments: ", frag_count)
+   msg_length = frag_count.to_bytes(2, 'big', signed=False)
+   client_socket.send(msg_length)
+   time.sleep(0.01)
+   byte_start = 0
+   byte_end = 1020
+   count = 0
+   for i in range(frag_count, 0, -1):
+      count_bytes = count.to_bytes(4, byteorder="big")
+      frag_msg = count_bytes + message[byte_start:byte_end]
+      # print(frag_msg, ";")
+      count += 1
+      byte_start += 1020
+      byte_end += 1020
+      client_socket.send(frag_msg)
+      time.sleep(0.01)
+
 
 # create a socket object that will listen
 server_socket = socket.socket(
@@ -40,15 +63,40 @@ while listenFlag:
    mode_bytes = client_socket.recv(1024)
    mode_received = mode_bytes.decode().lower()
    if mode_received == "ecb":
-      print("ecb mode")
       while True:
          # Receive the message from the client (receive no more than 1024 bytes)
-         msg_received = client_socket.recv(1024)
-
+         # msg_received = client_socket.recv(1024)
+         frag_num = client_socket.recv(1024)
+         # print(frag_num)
          # if there is no value for received bytes, no longer connected to client so break the while loop
-         if not msg_received:
+         # if not msg_received:
+         if not frag_num:
             break
          else:
+            msg_received = b''
+            frag_list = []
+            frag_num = int.from_bytes(frag_num, 'big', signed=False)
+            # print("Number of fragments: ", frag_num)
+            if frag_num > 1:
+               for i in range(0, frag_num):
+                  msg_received = client_socket.recv(1024)
+                  # print(msg_received)
+                  seq_bytes = msg_received[:4]
+                  seq_num = int.from_bytes(seq_bytes, byteorder='big')
+                  # print("sequence number: ", seq_num)
+                  frag_list.insert(seq_num, msg_received[4:])
+                  msg_received = b''
+                  # print(frag_list)
+               # print(frag_list)
+               for j in range(0, len(frag_list)):
+                  # print(j, ": ", frag_list[j])
+                  msg_received += frag_list[j]
+                  # print(msg_received)
+               # print(msg_received)
+               time.sleep(0.01)
+            else:
+               msg_received = client_socket.recv(1024)
+            # print(msg_received)
             cipher = AES.new(key_received, AES.MODE_ECB)
             pt_bytes = unpad(cipher.decrypt(msg_received), AES.block_size)
             received_plaintext = pt_bytes.decode()
@@ -58,22 +106,56 @@ while listenFlag:
          message_bytes = user_input.encode(encoding="utf-8")
          cipher = AES.new(key_received, AES.MODE_ECB)
          ct_bytes = cipher.encrypt(pad(message_bytes, AES.block_size))
-         ct_string = ct_bytes.decode(encoding="utf-8", errors="ignore")
-         print("key: ", key_received, "\nencrypted text: ", ct_string, "\n")
+         # ct_string = ct_bytes.decode(encoding="utf-8", errors="ignore")
+         # print("key: ", key_received, "\nencrypted text: ", ct_string, "\n")
          # Send the bytes through the connection socket
-         client_socket.send(ct_bytes)
+         if len(ct_bytes) > 1024:
+            fragment_message(ct_bytes)
+         else:
+            frag_count = 1
+            # print("Number of fragments: ", frag_count)
+            msg_length = frag_count.to_bytes(2, 'big', signed=False)
+            # print(msg_length)
+            client_socket.send(msg_length)
+            time.sleep(0.01)
+            client_socket.send(ct_bytes)
+         # client_socket.send(ct_bytes)
 
    elif mode_received == "cbc":
-      print("cbc mode")
       while True:
          iv_received = client_socket.recv(1024)
-         print("iv received: ", iv_received)
+         # print("iv received: ", iv_received)
          # Receive the message from the client (receive no more than 1024 bytes)
-         msg_received = client_socket.recv(1024)
+         frag_num = client_socket.recv(1024)
+         # print(frag_num)
          # if there is no value for received bytes, no longer connected to client so break the while loop
-         if not msg_received:
+         # if not msg_received:
+         if not frag_num:
             break
          else:
+            msg_received = b''
+            frag_list = []
+            frag_num = int.from_bytes(frag_num, 'big', signed=False)
+            # print("Number of fragments: ", frag_num)
+            if frag_num > 1:
+               for i in range(0, frag_num):
+                  msg_received = client_socket.recv(1024)
+                  # print(msg_received)
+                  seq_bytes = msg_received[:4]
+                  seq_num = int.from_bytes(seq_bytes, byteorder='big')
+                  # print("sequence number: ", seq_num)
+                  frag_list.insert(seq_num, msg_received[4:])
+                  msg_received = b''
+                  # print(frag_list)
+               # print(frag_list)
+               for j in range(0, len(frag_list)):
+                  # print(j, ": ", frag_list[j])
+                  msg_received += frag_list[j]
+                  # print(msg_received)
+               # print(msg_received)
+               time.sleep(0.01)
+            else:
+               msg_received = client_socket.recv(1024)
             cipher = AES.new(key_received, AES.MODE_CBC, iv_received)
             pt_bytes = unpad(cipher.decrypt(msg_received), AES.block_size)
             received_plaintext = pt_bytes.decode()
@@ -83,7 +165,17 @@ while listenFlag:
          message_bytes = user_input.encode(encoding="utf-8")
          cipher = AES.new(key_received, AES.MODE_CBC, iv_received)
          ct_bytes = cipher.encrypt(pad(message_bytes, AES.block_size))
-         ct_string = ct_bytes.decode(encoding="utf-8", errors="ignore")
-         print("key: ", key_received, "\nencrypted text: ", ct_string, "\n")
+         # ct_string = ct_bytes.decode(encoding="utf-8", errors="ignore")
+         # print("key: ", key_received, "\nencrypted text: ", ct_string, "\n")
          # Send the bytes through the connection socket
-         client_socket.send(ct_bytes)
+         if len(ct_bytes) > 1024:
+            fragment_message(ct_bytes)
+         else:
+            frag_count = 1
+            # print("Number of fragments: ", frag_count)
+            msg_length = frag_count.to_bytes(2, 'big', signed=False)
+            # print(msg_length)
+            client_socket.send(msg_length)
+            time.sleep(0.01)
+            client_socket.send(ct_bytes)
+         # client_socket.send(ct_bytes)
